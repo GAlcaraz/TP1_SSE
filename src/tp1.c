0ba1cbc0977946419a5ef82d922da19d500efe3c
GAlcaraz <g.alcaraz@outlook.com>
2018-04-19 21:35:53 -0300

    Agregados defines a tickHooks

--- src/tp1.c
+++ src/tp1.c
@@ -11,13 +11,15 @@
 
 #ifdef TP1_1
 
+
+
 int main(void){
    boardConfig();
    while(1) {
       gpioWrite( LEDB, ON );
-      delay(50);
+      delay(LED_TOGGLE_MS);
       gpioWrite( LEDB, OFF );
-      delay(50);
+      delay(LED_TOGGLE_MS);
    }
    return 0 ;
 }
@@ -57,6 +59,10 @@ int main(void){
 
 
 #ifdef TP1_3
+
+#define TICKRATE_MS	500	//10 100
+#define LED_TOGGLE_MS 1000 //	100	500
+
 /* FUNCION que se ejecuta cada vez que ocurre un Tick. */
 void myTickHook( void *ptr ){
 
@@ -84,7 +90,7 @@ int main(void){
    /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
       periodicamente una interrupcion cada 50ms que incrementa un contador de
       Ticks obteniendose una base de tiempos). */
-   tickConfig( 50 );
+   tickConfig( TICKRATE_MS );
 
    /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
       simplemente una funcion que se ejecutara peri�odicamente con cada
@@ -94,22 +100,22 @@ int main(void){
       al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
    */
    tickCallbackSet( myTickHook, (void*)LEDR );
-   delay(1000);
+   delay(LED_TOGGLE_MS);
 
    /* ------------- REPETIR POR SIEMPRE ------------- */
    while(1) {
       tickCallbackSet( myTickHook, (void*)LEDG );
-      delay(1000);
+      delay(LED_TOGGLE_MS);
       tickCallbackSet( myTickHook, (void*)LEDB );
-      delay(1000);
+      delay(LED_TOGGLE_MS);
       tickCallbackSet( myTickHook, (void*)LED1 );
-      delay(1000);
+      delay(LED_TOGGLE_MS);
       tickCallbackSet( myTickHook, (void*)LED2 );
-      delay(1000);
+      delay(LED_TOGGLE_MS);
       tickCallbackSet( myTickHook, (void*)LED3 );
-      delay(1000);
+      delay(LED_TOGGLE_MS);
       tickCallbackSet( myTickHook, (void*)LEDR );
-      delay(1000);
+      delay(LED_TOGGLE_MS);
    }
 
    /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
